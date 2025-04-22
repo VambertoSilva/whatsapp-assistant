248ea19b8363170b7e831ec15880893cf791393a
Vamberto <vambertosilva.vas@gmail.com>
2025-04-22 11:01:10 -0300

    chore(init): Initial project setup

---
+++ src/test/java/com/vamberto/whatsapp/assistant/WhatsappAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamberto.whatsapp.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
